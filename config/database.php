afb400cc2006be469aa971d68413b9820f786590
younes El Kasri <younes.elkasri@gmail.com>
2017-03-25 00:45:00 +0000

    Updated database configuration

--- config/database.php
+++ config/database.php
@@ -25,7 +25,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'pgsql'),
+    'default' => env('DB_CONNECTION', 'sqlite'),
 
     /*
     |--------------------------------------------------------------------------
