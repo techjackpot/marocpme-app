a50b377b67ebf42b80c57ee557ce324c65ea6cec
younes El Kasri <younes.elkasri@gmail.com>
2017-03-26 21:39:23 +0100

    generate seeds data

--- database/seeds/appointement.php
+++ database/seeds/appointement.php
@@ -13,28 +13,32 @@ class appointement extends Seeder
      */
     public function run()
     {
-        /*DB::table('appointments')->insert([
-            'name' => str_random(10),
-            'email' => str_random(10).'@gmail.com',
-            'password' => bcrypt('secret'),
-        ]);*/
-
-       /* DB::table('appointments')->insert([
-            'calendar_id' => 2,
-            'mailProspect' => str_random(10) . '@gmail.com',
-            'date' => Carbon::now(),
-            'note' => bcrypt('secret'),
-            'hour' => Carbon::createFromFormat('Y-m-d H', '2017-03-4 22')->toDateTimeString(),
-            'emplacement' => 'Marrakech',
-
-        ]);*/
+    //     DB::table('appointments')->insert([
+    //         'name' => str_random(10),
+    //         'email' => str_random(10).'@gmail.com',
+    //         'password' => bcrypt('secret'),
+    //     ]);
+       //
+    //    DB::table('appointments')->insert([
+    //         'calendar_id' => 2,
+    //         'mailProspect' => str_random(10) . '@gmail.com',
+    //         'date' => Carbon::now(),
+    //         'note' => bcrypt('secret'),
+    //         'hour' => Carbon::createFromFormat('Y-m-d H', '2017-03-4 22')->toDateTimeString(),
+    //         'emplacement' => 'Marrakech',
+       //
+    //     ]);
 
         DB::table('users')->insert([
             'password' => bcrypt("123456"),
             'nom'=>'nadir',
             'prenom'=>'anass',
             'email' => 'anass@gmail.com',
-            'isAdmin' => 'heIS',
+            'tel' => '+233999394934',
+            'poste' => 'kfkdkfd',
+            'localisation' => 'marrakech',
+            //'isAdmin' => 'heIS',
+            'active' => 'forNow'
         ]);
             }
 }
