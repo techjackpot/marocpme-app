91502f55e9e7eae7b54d5996b0d76eea5d693e33
Mr.Jack <techjack117@outlook.com>
2017-04-05 20:19:41 +0800

    update time picker forgotten

--- resources/views/prospects/details.blade.php
+++ resources/views/prospects/details.blade.php
@@ -590,12 +590,13 @@
                                                     <div class="form-group">
                                                         <label for="inpHour">Heure</label>
                                                         <input type="text" class="form-control inpHour" name="hourM" id="inpHour">
-                                                        <span> <img src="" alt=""></span>
+                                                        <span> <img src="{{asset('img/prospect/hour.png')}}" alt=""></span>
 
                                                     </div>
                                                     <div class="form-group">
                                                         <label for="inpEmp">Emplacement</label>
                                                         <input type="text" class="form-control" name="empM" id="inpEmp">
+                                                        <span> <img src="{{asset('img/prospect/emplacement.png')}}" alt=""></span>
                                                     </div>
                                                     <div class="form-group">
                                                         <label for="inpN">Note</label>
